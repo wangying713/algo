058f94b50ff620deb26d617aefa9668d455988f6
王影 <isme.jac@gmail.com>
2020-02-21 00:55:57 +0800

    练习题

--- test/BucketSort.php
+++ test/BucketSort.php
@@ -1,36 +1,34 @@
 <?php
 
+/**
+ * 桶排序
+ */
 function buckerSort(&$arr, $bucketSize = 0)
 {
     $count = count($arr);
-    if ($count < 2) return;
-
-    // 桶的大小
     if ($bucketSize == 0) $bucketSize = $count;
 
+    // 用来计算最大最小区间
     $min = min($arr);
-    $max = max($arr);
-
-    $buckets =  [];
-    // 将数组分配到各个桶中
+    $buckets = [];
     for ($i = 0; $i < $count; $i++) {
         $index = ceil(($arr[$i] - $min) / $bucketSize);
         $buckets[$index][] = $arr[$i];
     }
+    $bucketCount = count($buckets);
 
     $k = 0;
-    // 桶的数量
-    $bucketCount = count($buckets);
     for ($i = 0; $i < $bucketCount; $i++) {
-        // 对每个桶进行排序，这里使用了快速排序
+        if (!is_array($buckets[$i])) continue;
         sort($buckets[$i]);
         foreach ($buckets[$i] as $val) {
-            $arr[$k++] = $val;
+            $arr[$k] = $val;
+            $k++;
         }
     }
 }
 
-$numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+$numbers = [15, 16, 17, 18, 19, 7, 8, 9, 10, 11, 1, 2, 3, 4, 5, 6, 12, 13, 14, 20];
 $size = 10;
 $rs = buckerSort($numbers); //加载了quickSort文件，请忽略前几个打印
 
